564dd3970dda6c5fffedd67fa6603e045641942f
oilpal <jihoonl@gmail.com>
2021-08-29 17:37:36 +0900

    plenty/some/few/empty 적용

--- lib/main.dart
+++ lib/main.dart
@@ -99,10 +99,59 @@ class _MyHomePageState extends State<MyHomePage> {
           children: stores.map((e) {
             return ListTile(title: Text(e?.name ?? 'No title곳'),
               subtitle: Text(e?.addr ?? 'No address'),
-              trailing: Text(e?.remainStat ?? '매진'),
+              trailing: _buildRemainStatWidget(e),
               );
           }).toList(),
         ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(Store? store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+
+    if (store?.remainStat == 'plenty') {
+      remainStat = '충분';
+      description = '100개 이상';
+      color = Colors.green;
+    } else if (store?.remainStat == 'plenty') {
+      remainStat = '충분';
+      description = '100개 이상';
+      color = Colors.green;
+    }
+
+    switch(store?.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+      default:
+        break;
+    }
+
+    return Column(
+     children: <Widget>[
+       Text(remainStat, style: TextStyle(color: color, fontWeight: FontWeight.bold)
+       ),
+       Text(description, style: TextStyle(color: color))
+     ],
     );
   }
 
