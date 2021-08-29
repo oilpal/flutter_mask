74de01dc341ff331ce05a127a4f2f02b540f9d32
oilpal <jihoonl@gmail.com>
2021-08-29 19:15:56 +0900

    remain_stat_list_title.dart o.k

--- lib/ui/view/main_page.dart
+++ lib/ui/view/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mask/model/store.dart';
+import 'package:flutter_mask/ui/widget/remain_stat_list_tile.dart';
 import 'package:flutter_mask/viewmodel/store_model.dart';
 import 'package:provider/provider.dart';
 
@@ -23,59 +24,10 @@ class MainPage extends StatelessWidget {
             .map((e) {
           return ListTile(title: Text(e?.name ?? 'No title곳'),
             subtitle: Text(e?.addr ?? 'No address'),
-            trailing: _buildRemainStatWidget(e),
+            trailing: RemainStatListTile(e),
           );
         }).toList(),
       ),
-    );
-  }
-
-  Widget _buildRemainStatWidget(Store? store) {
-    var remainStat = '판매중지';
-    var description = '판매중지';
-    var color = Colors.black;
-
-    if (store?.remainStat == 'plenty') {
-      remainStat = '충분';
-      description = '100개 이상';
-      color = Colors.green;
-    } else if (store?.remainStat == 'plenty') {
-      remainStat = '충분';
-      description = '100개 이상';
-      color = Colors.green;
-    }
-
-    switch(store?.remainStat) {
-      case 'plenty':
-        remainStat = '충분';
-        description = '100개 이상';
-        color = Colors.green;
-        break;
-      case 'some':
-        remainStat = '보통';
-        description = '30 ~ 100개';
-        color = Colors.yellow;
-        break;
-      case 'few':
-        remainStat = '부족';
-        description = '2 ~ 30개';
-        color = Colors.red;
-        break;
-      case 'empty':
-        remainStat = '소진임박';
-        description = '1개 이하';
-        color = Colors.grey;
-        break;
-      default:
-        break;
-    }
-
-    return Column(
-      children: <Widget>[
-        Text(remainStat, style: TextStyle(color: color, fontWeight: FontWeight.bold)
-        ),
-        Text(description, style: TextStyle(color: color))
-      ],
     );
   }
 
